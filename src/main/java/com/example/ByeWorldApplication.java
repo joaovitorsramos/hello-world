0a2d525eb78c4b184dd5373dd7edde91f1bf42cf
joaovitorsramos <joaovitor-soares@hotmail.com>
2020-07-01 01:34:55 -0300

    first commit

---
+++ src/main/java/com/example/ByeWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByeWorldApplication.class, args);
+	}
+
+}
